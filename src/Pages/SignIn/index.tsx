4c9fa4b60643de90e80b03742515f03e723bb6e8
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-30 18:43:13 -0300

    feat: login working

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
+import { LoginForm } from "../../Components/LoginForm"
+
 export const SignInPage = () => {
     return(
         <div>
-            login
+            <LoginForm/>
         </div>
     )
 }
