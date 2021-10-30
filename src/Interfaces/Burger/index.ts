4621dfbc4f62d64a024ef562b8c26b1b20a804de
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-30 18:38:29 -0300

    feat: first interfaces created

---
+++ src/Interfaces/Burger/index.ts
@@ -0,0 +1,7 @@
+export interface IBurger {
+    "id": number;
+    "name": string;
+    "category": string;
+    "price": number;
+    // "userId": number
+}
---
+++ src/Interfaces/UserData/index.ts
@@ -0,0 +1,9 @@
+export interface IUserDataLogin {
+    email: string;
+    password: string;
+}
+export interface IUserDataRegister {
+    email: string;
+    name: string;
+    password: string;
+}
