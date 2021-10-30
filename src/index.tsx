91bc05c8d85f04994d9a2fd3ba1287677f22673f
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-30 18:41:30 -0300

    refactor: imported providers

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import Providers from "./Providers";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+     <Providers>
       <App />
+      </Providers>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
