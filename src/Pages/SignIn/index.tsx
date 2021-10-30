721d6b0ed4bbc79805d8c3eaf441d5d0a76457b1
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-30 19:57:23 -0300

    feat: added link to register page

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,9 +1,13 @@
 import { LoginForm } from "../../Components/LoginForm"
-
+import { Link } from "react-router-dom"
 export const SignInPage = () => {
     return(
         <div>
             <LoginForm/>
+            <div>
+                <small>Não possui uma conta?</small>
+                <Link to="/register">registre-se!</Link>
+            </div>
         </div>
     )
 }
