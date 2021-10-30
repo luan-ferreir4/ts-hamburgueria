930d0441510c56c212db5c1bea3d08ffb43e4795
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-30 20:00:58 -0300

    feat: register working

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -1,7 +1,13 @@
+import { RegisterForm } from "../../Components/RegisterForm"
+import { Link } from "react-router-dom"
 export const RegisterPage = () => {
     return(
         <div>
-            Register
+            <RegisterForm/>
+            <div>
+                <small>Já possui uma conta?</small>
+                <Link to="/">Faça login!</Link>
+            </div>
         </div>
     )
 }
