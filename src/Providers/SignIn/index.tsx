6242f8e2765abc1604df53bd41edf71947769518
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-30 21:34:26 -0300

    feat: logout function and auth state

--- src/Providers/SignIn/index.tsx
+++ src/Providers/SignIn/index.tsx
@@ -1,26 +1,45 @@
 import axios from "axios";
 import { createContext, useContext, useState } from "react";
+import { useHistory } from "react-router";
 
 import { IProvidersProps } from "../../Interfaces/ProvidersProps";
 import { IUserDataLogin } from "../../Interfaces/UserData";
 
 interface ISignInProviderData {
-    signIn: (data: IUserDataLogin) => void;
+  signIn: (data: IUserDataLogin) => void;
+  signOut: () => void;
+  auth: string;
 }
 
 const SingInContext = createContext<ISignInProviderData>({} as ISignInProviderData);
 
 export const SignInProvider = ({children}: IProvidersProps) => {
+  const history = useHistory();
+
+  const [ auth, setAuth ] = useState("");
+
   const signIn = (data: IUserDataLogin) => {
     axios
       .post("https://hamburg-burguer-api.herokuapp.com/signin", data)
       .then(
-          res => console.log(res)
+          (res) => {
+            setAuth(JSON.stringify(res.data.accessToken))
+
+            localStorage.setItem("token", JSON.stringify(res.data.accessToken))
+
+            history.push("/home")
+          } 
       )
       .catch(err => console.log(err));
   };
 
-  return <SingInContext.Provider value={{signIn}}>
+  const signOut = () => {
+    localStorage.clear();
+    setAuth("");
+    history.push("/")
+  }
+
+  return <SingInContext.Provider value={{signIn, signOut, auth}}>
       {children}
   </SingInContext.Provider>;
 };
