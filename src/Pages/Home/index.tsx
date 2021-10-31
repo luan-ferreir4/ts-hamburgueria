bba7032cbabc4105f9aca915e763d13e136a055e
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-30 21:35:30 -0300

    feat: route protection and logout button

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,18 @@
+import { useHistory } from "react-router";
+import { UseSignIn } from "../../Providers/SignIn"
+
 export const HomePage = () => {
+    const history = useHistory(); 
+    
+    const  { signOut, auth } = UseSignIn();
+    if(auth === ""){
+       history.push("/")
+    }
+
     return(
         <div>
             HOme
+            <button onClick={signOut}>Sair</button>
         </div>
     )
 }
