6f1a288dda8d62c5deb84708d597c395d3896c1b
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-31 00:32:43 -0300

    refactor: providers refactoring

--- src/Providers/Register/index.tsx
+++ src/Providers/Register/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 
 import { IProvidersProps } from "../../Interfaces/ProvidersProps";
 import { IUserDataRegister } from "../../Interfaces/UserData";
--- src/Providers/SignIn/index.tsx
+++ src/Providers/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 import { useHistory } from "react-router";
 
 import { IProvidersProps } from "../../Interfaces/ProvidersProps";
@@ -8,40 +8,39 @@ import { IUserDataLogin } from "../../Interfaces/UserData";
 interface ISignInProviderData {
   signIn: (data: IUserDataLogin) => void;
   signOut: () => void;
-  auth: string;
 }
 
-const SingInContext = createContext<ISignInProviderData>({} as ISignInProviderData);
+const SingInContext = createContext<ISignInProviderData>(
+  {} as ISignInProviderData
+);
 
-export const SignInProvider = ({children}: IProvidersProps) => {
+export const SignInProvider = ({ children }: IProvidersProps) => {
   const history = useHistory();
-
-  const [ auth, setAuth ] = useState("");
+  const signOut = () => {
+    localStorage.clear();
+    history.push("/");
+  };
 
   const signIn = (data: IUserDataLogin) => {
     axios
       .post("https://hamburg-burguer-api.herokuapp.com/signin", data)
-      .then(
-          (res) => {
-            setAuth(JSON.stringify(res.data.accessToken))
+      .then((res) => {
+        localStorage.clear();
+        
+        localStorage.setItem("token", JSON.stringify(res.data.accessToken));
 
-            localStorage.setItem("token", JSON.stringify(res.data.accessToken))
-
-            history.push("/home")
-          } 
-      )
-      .catch(err => console.log(err));
+        localStorage.setItem("username", JSON.stringify(res.data.user.name));
+      
+        history.push("/home");
+      })
+      .catch((err) => console.log(err));
   };
 
-  const signOut = () => {
-    localStorage.clear();
-    setAuth("");
-    history.push("/")
-  }
-
-  return <SingInContext.Provider value={{signIn, signOut, auth}}>
+  return (
+    <SingInContext.Provider value={{ signIn, signOut }}>
       {children}
-  </SingInContext.Provider>;
+    </SingInContext.Provider>
+  );
 };
 
-export const UseSignIn = () => useContext(SingInContext); 
+export const UseSignIn = () => useContext(SingInContext);
